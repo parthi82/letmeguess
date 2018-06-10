38f0656a18446d87e5933c64cf4cdacaad84e4de
parthibhan kp <parthibhank82@gmail.com>
2018-06-10 23:23:40 +0530

    fix

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: path.join(__dirname, './ts/App.tsx'),
     output: {
         filename: 'app.js',
-        path: __dirname
+        path: path.join(__dirname, '../priv/static/js/dist/'),
     },
     module: {
         rules: [
