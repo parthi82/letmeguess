a08ebeaf668ba538e84526d301a221c8640393f8
parthibhan kp <parthibhank82@gmail.com>
2018-09-24 12:03:13 +0530

    fix drawing area

--- assets/js/components/Canvas.jsx
+++ assets/js/components/Canvas.jsx
@@ -5,6 +5,7 @@ let ctx;
 class Canvas extends Component {
   constructor(props) {
     super(props);
+    this.isDrawing = false;
     this.canvas = React.createRef();
     this.state = {};
   }
@@ -13,13 +14,49 @@ class Canvas extends Component {
     ctx = this.canvas.current.getContext('2d');
   }
 
-  draw(event) {
-    ctx.strokeStyle = 'black';
-    ctx.lineJoin = 'round';
-    ctx.lineWidth = 5;
-    ctx.lineTo(event.pageX, event.pageY);
+  getX(event) {
+		if (event.pageX === undefined) {
+			return event.targetTouches[0].pageX - this.canvas.offsetLeft;
+		}
+		else {
+			return event.pageX - this.canvas.offsetLeft;
+		}
+  }
+  
+  getY(event) {
+		if (event.pageY === undefined) {
+			return event.targetTouches[0].pageY - this.canvas.offsetTop;
+		}
+		else {
+			return event.pageY - this.canvas.offsetTop;
+		}
+  }
+  
+  start(event) {
+    this.isDrawing = true;
+    ctx.beginPath();
+    ctx.moveTo(this.getX(event), this.getY(event));
     event.preventDefault();
-    ctx.stroke();
+  }
+
+  end(event) {
+		if (this.isDrawing) {
+			ctx.stroke();
+			ctx.closePath();
+			this.isDrawing = false;
+		}
+		event.preventDefault();
+	}
+
+  draw(event) {
+    if (this.isDrawing) {
+      ctx.strokeStyle = 'black';
+      ctx.lineJoin = 'round';
+      ctx.lineWidth = 5;
+      ctx.lineTo(event.pageX, event.pageY);
+      event.preventDefault();
+      ctx.stroke();
+    }
   }
 
   render() {
@@ -28,6 +65,8 @@ class Canvas extends Component {
         ref={this.canvas}
         width={600}
         height={600}
+        onMouseUp={(e) => this.end(e)}
+        onMouseDown={(e) => this.start(e)}
         onMouseMove={(e) => this.draw(e)}
         style={{
           border: '2px solid black',
